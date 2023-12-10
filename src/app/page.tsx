40e102bd4e2b56c80aaedcf5ea68e90dc48281bd
hwalk1 <haydenjwalker1@gmail.com>
2023-12-10 13:12:29 +1100

    H1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,9 @@ export default function Page({}) {
           minHeight: "100vh",
         }}
       >
-        <h1>Hello, Zenlog</h1>
+        <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
+          Hello, Zenlog
+        </h1>
         <div
           style={{
             display: "flex",

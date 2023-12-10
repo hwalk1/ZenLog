ee29933321e2b661c5e71da2e7c60cfc9a5bdf0a
hwalk1 <haydenjwalker1@gmail.com>
2023-12-10 13:08:48 +1100

    Login Button TW

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,13 +24,15 @@ export default function Page({}) {
           }}
         >
           <button
-            style={{ marginRight: "50px" }}
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
             onClick={() => router.push("/journal")}
           >
             {" "}
             Login
           </button>
-          <button>Sign Up</button>
+          <button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+            Sign Up
+          </button>
         </div>
       </div>
     </>

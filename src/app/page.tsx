8696c1a84e970fb161e70f03fc1202c5bcb27c30
hwalk1 <haydenjwalker1@gmail.com>
2023-12-11 21:37:48 +1100

    Login & Sign Up Final

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,21 +10,14 @@ export default function Page({}) {
         <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
           Hello, Zenlog
         </h1>
-        <div
-          style={{
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "flex-end",
-          }}
-        >
+        <div className="flex item-center justify-center">
           <button
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            className="bg-secondary text-primary hover:bg-blue-700 text-white font-bold py-2 px-4 rounded min-w-full mr-2"
             onClick={() => router.push("/journal")}
           >
-            {" "}
             Login
           </button>
-          <button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+          <button className="bg-primary hover:bg-blue-500 text-secondary font-semibold hover:text-white py-2 px-4 border-blue-500 hover:border-transparent rounded min-w-full">
             Sign Up
           </button>
         </div>

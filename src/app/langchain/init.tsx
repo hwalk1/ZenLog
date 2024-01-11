df69fcb8fb9451950c0a9c3e9e2e8df2fcdee5f8
hwalk1 <haydenjwalker1@gmail.com>
2024-01-11 15:20:37 +1100

    Meditation Training

--- src/app/langchain/init.tsx
+++ src/app/langchain/init.tsx
@@ -1,13 +1,27 @@
 import { ChatOpenAI } from "langchain/chat_models/openai";
+import { ChatPromptTemplate } from "@langchain/core/prompts";
+
 const chatModel = new ChatOpenAI({
   openAIApiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY,
 });
 
+const prompt = ChatPromptTemplate.fromMessages([
+  [
+    "system",
+    "You are a world class meditator, you understand peoples problems from their journal entries, and expertly give them a short meditaion.",
+  ],
+  ["user", "{input}"],
+]);
+
 export async function runChat(text) {
   try {
-    const data = await chatModel.invoke(text);
+    const chain = prompt.pipe(chatModel);
+    const data = await chain.invoke({
+      input: `${text}`,
+    });
 
     if (data) {
+      console.log(data);
       return data;
     } else {
       console.log("No data received from the chat model.");

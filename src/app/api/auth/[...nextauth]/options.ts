666b1f1230fe14842b361eb5050a88cf64f32ddb
hwalk1 <haydenjwalker1@gmail.com>
2024-01-22 16:07:21 +1100

    GoogleProvider

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -1,5 +1,5 @@
 import GitHubProvider from "next-auth/providers/github";
-import GoggleProvider from "next-auth/providers/google";
+import GoogleProvider from "next-auth/providers/google";
 
 export const options = {
   providers: [
@@ -7,6 +7,28 @@ export const options = {
       // @ts-ignore
       profile(profile) {
         console.log("profile github", profile);
+
+        let userRole = "GitHub User";
+        if (profile?.email == "haydenjwalker1@gmail.com") {
+          userRole = "admin";
+        }
+
+        return {
+          ...profile,
+          role: userRole,
+        };
+      },
+    }),
+    GoogleProvider({
+      // @ts-ignore
+      profile(profile) {
+        console.log("profile github", profile);
+
+        return {
+          ...profile,
+          id: profile.sub,
+          role: userRole,
+        };
       },
     }),
   ],

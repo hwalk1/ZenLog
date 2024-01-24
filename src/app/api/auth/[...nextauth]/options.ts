b16e748ff9cd9b90c3607c2cca70fe8f02d83b66
hwalk1 <haydenjwalker1@gmail.com>
2024-01-24 17:15:37 +1100

    Return for Callbacks

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -41,11 +41,13 @@ export const options = {
     }),
   ],
   callbacks: {
-    async jwt({token, user}) {
-      if (user) token.role = user.role
-    }
-    async session({session, token}) {
-      if (session?.user) session.user.role = token.role
-    }
-  }
+    async jwt({ token, user }) {
+      if (user) token.role = user.role;
+      return token;
+    },
+    async session({ session, token }) {
+      if (session?.user) session.user.role = token.role;
+      return session;
+    },
+  },
 };

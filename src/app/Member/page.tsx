168aec5565997350ff01476b2531cc71cd242e35
hwalk1 <haydenjwalker1@gmail.com>
2024-01-26 13:10:17 +1100

    Member !session redirect

--- src/app/Member/page.tsx
+++ src/app/Member/page.tsx
@@ -1,6 +1,14 @@
-import React from "react";
+import { getServerSession } from "next-auth";
+import { options } from "../api/auth/[...nextauth]/options";
+import { redirect } from "next/navigation";
 
-const Member = () => {
+const Member = async () => {
+  const session = await getServerSession(options);
+
+  if (!session) {
+    redirect("/api/auth/signin?callbackUrl=/Member");
+  }
+
   return (
     <div>
       <h1>Member Server Session</h1>

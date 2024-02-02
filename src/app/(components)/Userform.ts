36003a134fbbb47469fa9bd7296ffbceea169c9b
hwalk1 <haydenjwalker1@gmail.com>
2024-02-02 12:36:25 +1100

    headers Update

--- src/app/(components)/Userform.ts
+++ src/app/(components)/Userform.ts
@@ -7,6 +7,27 @@ const UserForm = () => {
   const router = useRouter();
   const [formData, setFormData] = useState<object>({});
   const [errorMessage, setErrorMessage] = useState<string>("");
+
+  const handleChange = (e) => {
+    const value = e.target.value;
+    const name = e.target.name;
+    setFormData((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setErrorMessage("");
+    const res = await fetch("/api/Users", {
+      method: "POST",
+      body: JSON.stringify({ formData }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  };
 };
 
 export default UserForm;

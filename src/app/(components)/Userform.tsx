64df6e3dc04bda65d75914f67458239ae30ad19b
hwalk1 <haydenjwalker1@gmail.com>
2024-02-03 20:55:37 +1100

    Name, Password, Email Labels & Inputs

--- src/app/(components)/Userform.tsx
+++ src/app/(components)/Userform.tsx
@@ -3,9 +3,16 @@
 import { useRouter } from "next/navigation";
 import React, { useState } from "react";
 
+interface FormData {
+  name?: string;
+  email?: string;
+  password?: string;
+  // other form fields...
+}
+
 const UserForm: React.FC = () => {
   const router = useRouter();
-  const [formData, setFormData] = useState({});
+  const [formData, setFormData] = useState<FormData>({});
   const [errorMessage, setErrorMessage] = useState("");
 
   const handleChange = (e) => {
@@ -38,8 +45,43 @@ const UserForm: React.FC = () => {
   };
 
   return (
-    
-  )
+    <>
+      <form
+        onSubmit={handleSubmit}
+        method="post"
+        className="flex flex-col gap-3 w-1/2"
+      >
+        <h1>Create New User</h1>
+        <label>Full Name</label>
+        <input
+          id="name"
+          name="name"
+          onChange={handleChange}
+          required={true}
+          value={formData.name}
+          className="m-2 bg-slate-400 rounded"
+        ></input>
+        <label>Email</label>
+        <input
+          id="email"
+          name="email"
+          onChange={handleChange}
+          required={true}
+          value={formData.email}
+          className="m-2 bg-slate-400 rounded"
+        ></input>
+        <label>Password</label>
+        <input
+          id="password"
+          name="password"
+          onChange={handleChange}
+          required={true}
+          value={formData.password}
+          className="m-2 bg-slate-400 rounded"
+        ></input>
+      </form>
+    </>
+  );
 };
 
 export default UserForm;

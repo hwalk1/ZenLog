19b8e5b37a9409dcbc76ff396e160708bfd25eb0
hwalk1 <haydenjwalker1@gmail.com>
2024-02-04 10:11:32 +1100

    UserData duplicate, missing email

--- src/app/api/Users/route.ts
+++ src/app/api/Users/route.ts
@@ -8,6 +8,25 @@ export async function POST(req) {
     const userData = body.formData;
 
     //Confirm data exists
+    if (!userData?.email || !userData.password) {
+      return NextResponse.json(
+        { message: "All fields are required" },
+        { status: 400 }
+      );
+    }
+    //Check for duplicate emails
+    const duplicate = await User.findOne({ email: userData.email })
+      .lean()
+      .exec();
+
+    if (duplicate) {
+      return NextResponse.json({ message: "Duplicate Email" }, { status: 409 });
+    }
+
+    const hashPassword = await bcrypt.hash(userData.password, 10)
+    userData.password = hashPassword;
+
+    await 
   } catch (error) {
     console.log(error);
     return NextResponse.json({ message: "Error", error }, { status: 500 });

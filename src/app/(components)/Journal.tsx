df97e20f352b56d3f07e8a589722760698270362
hwalk1 <haydenjwalker1@gmail.com>
2024-02-07 23:24:03 +1100

    Entires

--- src/app/(components)/Journal.tsx
+++ src/app/(components)/Journal.tsx
@@ -1,7 +1,27 @@
 import React from "react";
 
+const journalEntries = [
+  {
+    id: 1,
+    title: "Day 1 Reflection",
+    text: "Today was a good day. I spent some time meditating and felt very peaceful.",
+    meditation: "10 minutes of mindfulness meditation",
+  },
+  {
+    id: 2,
+    title: "Day 2 Reflection",
+    text: "Felt a bit anxious today, but after some deep breathing exercises, I was able to calm down.",
+    meditation: "5 minutes of deep breathing",
+  },
+  // Add more entries as needed
+];
+
 const Journal = () => {
-  return <div>Journal</div>;
+  return (
+    <>
+      <div>Journal</div>;
+    </>
+  );
 };
 
 export default Journal;

8cdf065fa387b4304a3dd886a2fe8f423762b53c
hwalk1 <haydenjwalker1@gmail.com>
2024-02-07 23:26:36 +1100

    History Page

---
+++ src/app/History/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Journal from "../(components)/Journal";
+
+const History = () => {
+  return (
+    <div>
+      <Journal />
+    </div>
+  );
+};
+
+export default History;
